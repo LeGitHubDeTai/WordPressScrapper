9c1fdbac28a09f4de019f2c8b6e2dd4a33d61b65
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-09-14 14:39:16 +0200

    v1.2

--- main.js
+++ main.js
@@ -3,46 +3,47 @@ const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
-// Configuration de l'API WordPress
 const baseUrl = process.env.WORDPRESS_URL;
-const apiRoute = '/wp-json/wp/v2';
-const articlesEndpoint = `${baseUrl}${apiRoute}/posts`;
-const pagesEndpoint = `${baseUrl}${apiRoute}/pages`;
-
 const OUTPUT = process.env.OUTPUT || 'dist';
 
-// Mettre tous les fichiers dans un dossier "dist"
 const buildFolder = path.join(__dirname, OUTPUT);
 if (!fs.existsSync(buildFolder)) {
     fs.mkdirSync(buildFolder);
 }
 
-// Fonction pour récupérer les données de l'API
 async function fetchData(endpoint) {
     try {
         const response = await axios.get(endpoint);
         return response.data;
     } catch (error) {
-        console.error(`Erreur lors de la récupération des données : ${error.message}`);
+        console.error(`Erreur lors de la récupération des données de ${endpoint} : ${error.message}`);
         throw error;
     }
 }
 
-// Fonction pour récupérer et sauvegarder les articles, pages et images
 async function main() {
     try {
-        // Récupérer les articles et les pages
-        const articles = await fetchData(articlesEndpoint);
-        const pages = await fetchData(pagesEndpoint);
+        // Récupérer les routes de l'API à partir de /wp-json
+        const routes = await fetchData(`${baseUrl}/wp-json`);
 
-        // Sauvegarder les articles et les pages au format JSON
-        fs.writeFileSync(path.join(buildFolder, 'articles.json'), JSON.stringify(articles, null, 2));
-        fs.writeFileSync(path.join(buildFolder, 'pages.json'), JSON.stringify(pages, null, 2));
-
-        console.log('Articles et pages récupérés et sauvegardés avec succès.');
-
+        for (const route in routes.routes) {
+            // Pour cet exemple, nous allons considérer que toutes les routes terminant par un `/`
+            // (comme /wp/v2/posts/) sont les routes que nous souhaitons extraire. 
+            // Adaptez selon vos besoins.
+            if (!route.endsWith(')')) {
+                try {
+                    const data = await fetchData(baseUrl + '/wp-json' + route);
+                    const outputFilename = route.split('/').filter(Boolean).join('-') + '.json'; // Converti /wp/v2/posts/ en wp-v2-posts.json
+                    fs.writeFileSync(path.join(buildFolder, outputFilename), JSON.stringify(data, null, 2));
+                    console.log(`Données de ${route} récupérées et sauvegardées avec succès.`);
+                } catch (err) {
+                    console.error(`Erreur lors de la récupération des données de la route ${route}: ${err.message}`);
+                }
+            }
+        }
+        
         // Récupérer les images
-        const mediaEndpoint = `${baseUrl}${apiRoute}/media`;
+        const mediaEndpoint = `${baseUrl}/wp-json/wp/v2/media`;
         const media = await fetchData(mediaEndpoint);
 
         // Créer un dossier pour les images s'il n'existe pas déjà
@@ -50,7 +51,7 @@ async function main() {
         if (!fs.existsSync(assetsFolder)) {
             fs.mkdirSync(assetsFolder);
         }
-
+        
         // Télécharger et sauvegarder les images
         for (const item of media) {
             const imageUrl = item.source_url;
@@ -71,9 +72,8 @@ async function main() {
             console.log(`Image ${imageName} téléchargée et sauvegardée.`);
         }
     } catch (error) {
-        console.error(`Une erreur s'est produite : ${error.message}`);
+        console.error(`Une erreur s'est produite lors de la récupération des routes: ${error.message}`);
     }
 }
 
-// Appeler la fonction principale
 main();
